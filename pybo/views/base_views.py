1d6071dfe24ef1a40337fdd1ba2a26cff64fb15a
chlendyd7 <chlendyd7@naver.com>
2023-05-23 16:49:57 +0900

    INFO 레벨로 출력"

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -8,6 +8,7 @@ logger = logging.getLogger('pybo')
 
 
 def index(request):
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw','') # 검색어
     question_list = Question.objects.order_by('-create_date')
