71742236bc070bc1df4df881db5ffb1f6121bf59
chlendyd7 <chlendyd7@naver.com>
2023-05-23 16:46:38 +0900

    log등록

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,12 +1,13 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-
+import logging
 from pybo.models import Question
+
+logger = logging.getLogger('pybo')
 
 
 def index(request):
-    3/0
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw','') # 검색어
     question_list = Question.objects.order_by('-create_date')
